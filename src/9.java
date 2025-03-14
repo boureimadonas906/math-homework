2f7bcf7e6ab94e7c6bf414831e4de52b75845ba4
boureimadonas906 <boureimadonas906@gmail.com>
2025-03-14 18:16:38 +0000

    Add 9.java

---
+++ src/9.java
@@ -0,0 +1,10 @@
+
+public class MathHomework {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int a = rand.nextInt(10);
+        int b = rand.nextInt(10);
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
