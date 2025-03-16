d3f91e904937188f30401d8dc6da58ef21e37bb4
boureimadonas906 <boureimadonas906@gmail.com>
2025-03-16 19:46:03 +0000

    Add 11.java

---
+++ src/11.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class mathHomework{
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    double a = s.nextDouble();
+    double b = s.nextDouble();
+    double c = s.nextDouble();
+    double d = s.nextDouble();
+    
+    // Your code goes here
+  }
+}
