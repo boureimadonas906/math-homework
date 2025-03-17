4921161ca973782fab94f811d9b36ac8f5393c7f
boureimadonas906 <boureimadonas906@gmail.com>
2025-03-17 09:53:12 +0000

    Add 12.java

---
+++ src/12.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class MathHomework {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int a = rand.nextInt(10);
+        int b = rand.nextInt(10);
+        System.out.println("Solve the equation:");
+        System.out.println(a + "x + " + b + " = ");
+    }
+}
