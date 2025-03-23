b69c60348e6c7e10eb2240829a2fd9a3dc49f509
boureimadonas906 <boureimadonas906@gmail.com>
2025-03-23 22:05:17 +0000

    Add 17.java

---
+++ src/17.java
@@ -0,0 +1,13 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int sum = 0;
+        double avg = 0.0;
+
+        // Example: Add two numbers and calculate their average
+        sum += 5; // 1 + 4 = 5
+        avg = (double)(sum / 2); // 5 / 2 = 2.5
+
+        System.out.println("Sum of the numbers is " + sum);
+        System.out.println("Average of the numbers is " + avg);
+    }
+}
