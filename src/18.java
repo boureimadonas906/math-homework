6fa68dc01964f81f41bc213c037bd2a3206c6c6e
boureimadonas906 <boureimadonas906@gmail.com>
2025-03-24 05:45:22 +0000

    Add 18.java

---
+++ src/18.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code for performing basic arithmetic operations
+        int result = 5 + 3;
+        System.out.println("The sum of 5 and 3 is: " + result);
+        
+        double area = 8.0 * 4.0 / 2.0; // Area calculation example
+        System.out.println("The area of the rectangle with sides 8m x 4m is: " + area);
+    }
+}
