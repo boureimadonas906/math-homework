9725a56374defc4c74766ca79b3cc38cb4e7cfa5
boureimadonas906 <boureimadonas906@gmail.com>
2025-03-31 02:45:28 +0000

    Add 21.java

---
+++ src/21.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 3.14;
+        String z = "Hello World";
+        System.out.println("The value of x is " + x);
+        System.out.println("The value of y is " + y);
+        System.out.println("String z is " + z);
+    }
+}
