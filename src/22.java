6aa897587ee27f492444fc05cbdf5fbfe186849f
boureimadonas906 <boureimadonas906@gmail.com>
2025-04-01 14:34:58 +0000

    Add 22.java

---
+++ src/22.java
@@ -0,0 +1,20 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of basic arithmetic operations
+        int result = 5 + 3;
+        System.out.println("The sum of 5 and 3 is: " + result);
+
+        // Example of basic comparison
+        boolean isEqual = true;
+        if (isEqual) {
+            System.out.println("The values are equal.");
+        } else {
+            System.out.println("The values are not equal.");
+        }
+
+        // Example of loops
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
