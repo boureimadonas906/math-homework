a74926fc00a8acc501359b213db4deafce30cb25
boureimadonas906 <boureimadonas906@gmail.com>
2025-04-02 00:05:05 +0000

    Add 23.java

---
+++ src/23.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of simple calculation
+        System.out.println("The sum is: " + (20.5 + 34.8));
+        
+        // Another example with variables
+        double a = 10, b = 20;
+        System.out.println("The result of adding " + a + " and " + b + " is: " + (a + b));
+    }
+}
