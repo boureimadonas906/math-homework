47d527ab9913699d57fa6d8550a5dcc28db2e445
boureimadonas906 <boureimadonas906@gmail.com>
2025-04-03 12:40:02 +0000

    Add 24.java

---
+++ src/24.java
@@ -0,0 +1,12 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+
+        if (num1 > num2) {
+            System.out.println("Num1 is greater than Num2");
+        } else {
+            System.out.println("Num2 is greater than Num1");
+        }
+    }
+}
