8260af17a8229bd307a88f5f05d28e5663df1284
boureimadonas906 <boureimadonas906@gmail.com>
2025-04-05 06:52:27 +0000

    Add 26.java

---
+++ src/26.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        double result = 5 + 3;
+        System.out.println(result);
+    }
+}
