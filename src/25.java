b3161496f880cf1c8114c68c3f20d64fdefa96c4
boureimadonas906 <boureimadonas906@gmail.com>
2025-04-05 05:38:07 +0000

    Add 25.java

---
+++ src/25.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code to solve an arithmetic problem
+        int num1 = 5;
+        int num2 = 3;
+        int result = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
