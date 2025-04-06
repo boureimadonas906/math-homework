6a40a5363e9eee5aa4c68e80513bcb9ec2607017
boureimadonas906 <boureimadonas906@gmail.com>
2025-04-06 05:29:21 +0000

    Add 27.java

---
+++ src/27.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        
+        // Perform some arithmetic operation
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
