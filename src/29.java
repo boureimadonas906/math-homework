127cb4f107c872eb25ba1257825e0e5b2b383c4e
boureimadonas906 <boureimadonas906@gmail.com>
2025-04-08 07:28:11 +0000

    Add 29.java

---
+++ src/29.java
@@ -0,0 +1,13 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 3.14;
+        String s = "Hello, World!";
+        boolean result = true;
+
+        System.out.println("The value of a is: " + a);
+        System.out.println("The value of b is: " + b);
+        System.out.println(s);
+        System.out.println(result);
+    }
+}
