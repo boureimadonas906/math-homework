cb4017601ef84d65b98d829f075f830e17b7e89c
boureimadonas906 <boureimadonas906@gmail.com>
2025-04-11 10:53:13 +0000

    Add 30.java

---
+++ src/30.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code for calculating area of a rectangle
+        double base = 5.0;
+        double height = 3.0;
+        double area = base * height;
+
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
