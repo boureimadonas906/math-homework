33ddcc8a8c3c25c314caee3ac27a0de02c4a4bcd
boureimadonas906 <boureimadonas906@gmail.com>
2025-04-23 10:34:09 +0000

    Add 31.java

---
+++ src/31.java
@@ -0,0 +1,20 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of some arithmetic operations
+        int sum = 5 + 3;
+        double product = 7.0 * 2.5;
+
+        System.out.println("The sum is: " + sum);
+        System.out.println("The product is: " + product);
+
+        // Another example using variable assignment and loops
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println("Even number " + i);
+        }
+
+        // More complex code like for or while loops, etc.
+    }
+}
